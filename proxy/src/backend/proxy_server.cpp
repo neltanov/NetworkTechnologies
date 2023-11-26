56ec2929a840556572f67a4261a9e05c167cee65
Bayarto Neltanov <bayarto2003@gmail.com>
2023-11-27 04:13:57 +0700

    added the ability to process multiple connections

--- proxy/src/backend/proxy_server.cpp
+++ proxy/src/backend/proxy_server.cpp
@@ -12,82 +12,89 @@ using boost::asio::ip::tcp;
 Socks5Proxy::Socks5Proxy(unsigned short proxy_port)
     : io_ctx(),
       client_acceptor(io_ctx, tcp::endpoint(tcp::v4(), proxy_port)),
-      client_socket(io_ctx) {
-        
-}
+      socket_pool(io_ctx) {}
 
 void Socks5Proxy::start() {
     acceptConnection();
     io_ctx.run();
 }
 
-
 void Socks5Proxy::acceptConnection() {
-    client_acceptor.async_accept(client_socket,
-        [this](const error_code& ec) {
+    auto client_socket_ptr = socket_pool.getSocket();
+    client_acceptor.async_accept(*client_socket_ptr,
+        [this, client_socket_ptr](const error_code& ec) {
             if (!ec) {
-                std::cout << "New connection: " << client_socket.remote_endpoint() << std::endl;
-                handleGreeting();
+                std::cout << "New connection: " << client_socket_ptr->remote_endpoint() << std::endl;
+                handleGreeting(client_socket_ptr);
             }
             acceptConnection();
         });
 }
 
-void Socks5Proxy::handleGreeting() {
+void Socks5Proxy::handleGreeting(std::shared_ptr<tcp::socket> client_socket_ptr) {
+    std::cout << "Handle greeting for " << client_socket_ptr->remote_endpoint() << std::endl;
+
     // Reading client greeting (SOCKS version and number of authentication methods supported)
-    async_read(client_socket, buffer(buffer_data, 2), bind(&Socks5Proxy::readAuthMethods, this, _1, _2));
+    async_read(*client_socket_ptr, buffer(buffer_data, 2), bind(&Socks5Proxy::readAuthMethods, this, _1, _2, client_socket_ptr));
 }
 
-void Socks5Proxy::readAuthMethods(const error_code& ec, std::size_t length) {
+void Socks5Proxy::readAuthMethods(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr) {
     if (!ec && length == 2 && buffer_data[0] == 5) {
         size_t count_of_methods = buffer_data[1];
         // Reading authentication methods
-        async_read(client_socket, buffer(buffer_data, count_of_methods), 
-                    bind(&Socks5Proxy::sendServerChoice, this, _1, _2, count_of_methods));
+        async_read(*client_socket_ptr, buffer(buffer_data, count_of_methods), 
+                    bind(&Socks5Proxy::sendServerChoice, this, _1, _2, count_of_methods, client_socket_ptr));
     }
     else {
         std::cerr << "Auth methods receiving error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
     }
 }
 
-void Socks5Proxy::sendServerChoice(const error_code& ec, std::size_t length, std::size_t count_of_methods) {
+void Socks5Proxy::sendServerChoice(const error_code& ec, std::size_t length, std::size_t count_of_methods, std::shared_ptr<tcp::socket> client_socket_ptr) {
+    std::cout << "sendServerChoice " << client_socket_ptr->remote_endpoint() << std::endl;
+    
     if (!ec && length == count_of_methods) {
         buffer_data[0] = 5;  // SOCKS5 version
         buffer_data[1] = 0;  // No authentification method
         // Sending server choice
-        async_write(client_socket, buffer(buffer_data, 2), bind(&Socks5Proxy::handleConnectionRequest, this, _1, _2));
+        async_write(*client_socket_ptr, buffer(buffer_data, 2), bind(&Socks5Proxy::handleConnectionRequest, this, _1, _2, client_socket_ptr));
     }
     else {
         std::cerr << "Server choice sending error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
     }
 }
 
-void Socks5Proxy::handleConnectionRequest(const error_code& ec, std::size_t) {
+void Socks5Proxy::handleConnectionRequest(const error_code& ec, std::size_t, std::shared_ptr<tcp::socket> client_socket_ptr) {
+    std::cout << "handleConnectionRequest " << client_socket_ptr->remote_endpoint() << std::endl;
     if (!ec) {
         // Reading client connection request (Firstly, SOCKS version, command code and reserved byte)
-        async_read(client_socket, buffer(buffer_data, 3), bind(&Socks5Proxy::readAddressType, this, _1, _2));
+        async_read(*client_socket_ptr, buffer(buffer_data, 3), bind(&Socks5Proxy::readAddressType, this, _1, _2, client_socket_ptr));
     }
     else {
         std::cerr << "Client connection request is failed: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
     }
 }
 
-void Socks5Proxy::readAddressType(const error_code& ec, std::size_t length) {
+void Socks5Proxy::readAddressType(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr) {
     if (!ec && length == 3 && buffer_data[0] == 5 && buffer_data[1] == 1 && buffer_data[2] == 0) {
         // Reading address type
-        async_read(client_socket, buffer(buffer_data, 1), bind(&Socks5Proxy::handleAddress, this, _1, _2));
+        async_read(*client_socket_ptr, buffer(buffer_data, 1), bind(&Socks5Proxy::handleAddress, this, _1, _2, client_socket_ptr));
     }
     else {
-        std::cerr << "Address reading error: " << ec.message() << std::endl;
+        std::cerr << "Address type reading error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
     }
 }
 
-void Socks5Proxy::handleAddress(const error_code& ec, std::size_t length) {
+void Socks5Proxy::handleAddress(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr) {
     if (!ec && length == 1) {
         uint8_t address_type = buffer_data[0];
         if (address_type == 1) { // IPv4 address
             // Read 4 bytes address and 2 bytes port
-            async_read(client_socket, buffer(buffer_data, 6), bind(&Socks5Proxy::readIPv4Address, this, _1, _2));
+            async_read(*client_socket_ptr, buffer(buffer_data, 6), bind(&Socks5Proxy::readIPv4Address, this, _1, _2, client_socket_ptr));
         }
         else if (address_type == 3) { // TODO: DNS resolving
             std::cout << "DNS resolving is not done yet" << std::endl;
@@ -97,9 +104,13 @@ void Socks5Proxy::handleAddress(const error_code& ec, std::size_t length) {
             std::cout << "IPv6 is not supported" << std::endl;
         }
     }
+    else {
+        std::cerr << "Address handling error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
+    }
 }
 
-void Socks5Proxy::readIPv4Address(const error_code& ec, std::size_t length) {
+void Socks5Proxy::readIPv4Address(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr) {
     if (!ec && length == 6) {
         ip::address_v4::bytes_type ipAddressBytes;
         memcpy(ipAddressBytes.data(), &buffer_data[0], 4);
@@ -110,13 +121,17 @@ void Socks5Proxy::readIPv4Address(const error_code& ec, std::size_t length) {
         port = ntohs(port);
         std::cout << "Remote server: " << ip.to_string() << ":" << port << std::endl;
 
-        auto remote_socket = std::make_shared<tcp::socket>(client_socket.get_executor());
+        auto remote_socket = std::make_shared<tcp::socket>(io_ctx);
         
-        remote_socket->async_connect(tcp::endpoint(ip, port), bind(&Socks5Proxy::sendServerResponse, this, _1, remote_socket));
+        remote_socket->async_connect(tcp::endpoint(ip, port), bind(&Socks5Proxy::sendServerResponse, this, _1, client_socket_ptr, remote_socket));
+    }
+    else {
+        std::cerr << "IPv4 address reading error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
     }
 }
 
-void Socks5Proxy::sendServerResponse(const error_code& ec, std::shared_ptr<tcp::socket> remote_socket) {
+void Socks5Proxy::sendServerResponse(const error_code& ec, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket) {
     if (!ec) {
         std::cout << "Proxy connected to server " << remote_socket->remote_endpoint() << " successfully" << std::endl;
         // Response packet from server
@@ -124,45 +139,57 @@ void Socks5Proxy::sendServerResponse(const error_code& ec, std::shared_ptr<tcp::
         buffer_data[1] = 0;  // Success
         buffer_data[2] = 0;  // Reserved byte
         buffer_data[3] = 1;  // Address type (IPv4)
-        async_write(client_socket, buffer(buffer_data, 10), 
-                    bind(&Socks5Proxy::startDataTransfer, this, _1, _2, remote_socket));
+        async_write(*client_socket_ptr, buffer(buffer_data, 10), 
+                    bind(&Socks5Proxy::startDataTransfer, this, _1, _2, client_socket_ptr, remote_socket));
     }
     else {
-        std::cerr << ec.message() << std::endl;
+        std::cerr << "Server response sending error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
+        error_code er_code;
+        remote_socket->shutdown(tcp::socket::shutdown_both, er_code);
+        remote_socket->close(er_code);
     }
 }
 
-void Socks5Proxy::startDataTransfer(const error_code& ec, std::size_t, std::shared_ptr<tcp::socket> remote_socket) {
+void Socks5Proxy::startDataTransfer(const error_code& ec, std::size_t, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket) {
     if (!ec) {
-        std::cout << "Data transfer from client " << client_socket.remote_endpoint() << std::endl;
-        client_socket.async_read_some(buffer(buffer_data, max_length), bind(&Socks5Proxy::sendDataToServer, this, _1, _2, remote_socket));
+        std::cout << "Data transfer from client " << client_socket_ptr->remote_endpoint() << std::endl;
+        client_socket_ptr->async_read_some(buffer(buffer_data, max_length), bind(&Socks5Proxy::sendDataToServer, this, _1, _2, client_socket_ptr, remote_socket));
 
         std::cout << "Data transfer from server " << remote_socket->remote_endpoint() << std::endl;
-        remote_socket->async_read_some(buffer(buffer_data, max_length), bind(&Socks5Proxy::sendDataToClient, this, _1, _2, remote_socket));
+        remote_socket->async_read_some(buffer(buffer_data, max_length), bind(&Socks5Proxy::sendDataToClient, this, _1, _2, client_socket_ptr, remote_socket));
     }
     else {
-        std::cerr << ec.message() << std::endl;
+        std::cerr << "Start data transfer error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
+        error_code er_code;
+        remote_socket->shutdown(tcp::socket::shutdown_both, er_code);
+        remote_socket->close(er_code);
     }
 }
 
-void Socks5Proxy::sendDataToServer(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> remote_socket) {
+void Socks5Proxy::sendDataToServer(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket) {
     if (!ec) {
         std::cout << "Data transfer to server " << remote_socket->remote_endpoint() << std::endl;
-        async_write(*remote_socket, buffer(buffer_data, length), bind(&Socks5Proxy::startDataTransfer, this, _1, _2, remote_socket));
+        async_write(*remote_socket, buffer(buffer_data, length), bind(&Socks5Proxy::startDataTransfer, this, _1, _2, client_socket_ptr, remote_socket));
     } else {
-        // client_socket.close();
-        // remote_socket->close();
-        std::cout << "async_read_some from client socket in startDataTransfer error: " << ec.message() << std::endl; 
+        std::cout << "async_read_some from client socket in startDataTransfer error: " << ec.message() << std::endl;
+        socket_pool.addSocket(client_socket_ptr);
+        error_code er_code;
+        remote_socket->shutdown(tcp::socket::shutdown_both, er_code);
+        remote_socket->close(er_code);
     }
 }
 
-void Socks5Proxy::sendDataToClient(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> remote_socket) {
+void Socks5Proxy::sendDataToClient(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket) {
     if (!ec) {
-        std::cout << "Data transfer to client " << client_socket.remote_endpoint() << std::endl;
-        async_write(client_socket, buffer(buffer_data, length), bind(&Socks5Proxy::startDataTransfer, this, _1, _2, remote_socket));
+        std::cout << "Data transfer to client " << client_socket_ptr->remote_endpoint() << std::endl;
+        async_write(*client_socket_ptr, buffer(buffer_data, length), bind(&Socks5Proxy::startDataTransfer, this, _1, _2, client_socket_ptr, remote_socket));
     } else {
-        // client_socket.close();
-        // remote_socket->close();
         std::cout << "async_read_some from remote socket in startDataTransfer error: " << ec.message() << std::endl; 
+        socket_pool.addSocket(client_socket_ptr);
+        error_code er_code;
+        remote_socket->shutdown(tcp::socket::shutdown_both, er_code);
+        remote_socket->close(er_code);
     }
 }
--- proxy/src/include/proxy_server.h
+++ proxy/src/include/proxy_server.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
+#include "socket_pool.h"
 
 using namespace boost::asio;
 using namespace boost::system;
@@ -18,22 +19,24 @@ public:
 private:
     io_context io_ctx;
     tcp::acceptor client_acceptor;
-    tcp::socket client_socket;
+    // tcp::socket client_socket;
+    SocketPool socket_pool;
+
     static const int max_length = 4096;
     char buffer_data[max_length];
 
     void acceptConnection();
-    void handleGreeting();
-    void readAuthMethods(const error_code& ec, std::size_t length);
-    void sendServerChoice(const error_code& ec, std::size_t length, std::size_t count_of_methods);
-    void handleConnectionRequest(const error_code& ec, std::size_t length);
-    void readAddressType(const error_code& ec, std::size_t length);
-    void handleAddress(const error_code& ec, std::size_t length);
-    void readIPv4Address(const error_code& ec, std::size_t length);
-    void sendServerResponse(const error_code& ec, std::shared_ptr<tcp::socket> remote_socket);
-    void startDataTransfer(const error_code& ec, std::size_t, std::shared_ptr<tcp::socket> remote_socket);
-    void sendDataToServer(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> remote_socket);
-    void sendDataToClient(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> remote_socket);
+    void handleGreeting(std::shared_ptr<tcp::socket> socket_ptr);
+    void readAuthMethods(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void sendServerChoice(const error_code& ec, std::size_t length, std::size_t count_of_methods, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void handleConnectionRequest(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void readAddressType(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void handleAddress(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void readIPv4Address(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void sendServerResponse(const error_code& ec, std::shared_ptr<tcp::socket> remote_socket, std::shared_ptr<tcp::socket> client_socket_ptr);
+    void startDataTransfer(const error_code& ec, std::size_t, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket);
+    void sendDataToServer(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket);
+    void sendDataToClient(const error_code& ec, std::size_t length, std::shared_ptr<tcp::socket> client_socket_ptr, std::shared_ptr<tcp::socket> remote_socket);
 };
 
 #endif // PROXY_SERVER_H
